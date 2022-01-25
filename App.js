30085a0a2de1bef4cc541bba1735bf0080ab7bbd
Dennis Nyamwamu <dnyamwamu@gmail.com>
2022-01-25 16:46:45 +0300

    full restaurant app

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
+import React from 'react'
+import { ThemeProvider } from 'styled-components/native'
+import * as firebase from 'firebase'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald'
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
+
+import { theme } from './src/infrastructure/theme'
+import { Navigation } from './src/infrastructure/navigation'
+
+import { AuthenticationContextProvider } from './src/services/authentication/authentication.context'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA9GrH4hGxtKTCGDMzYnVA7JvdDdS5EXJo',
+  authDomain: 'mamasafisha.firebaseapp.com',
+  projectId: 'mamasafisha',
+  storageBucket: 'mamasafisha.appspot.com',
+  messagingSenderId: '954807759502',
+  appId: '1:954807759502:web:13ac404ff01dff11894fdf',
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  })
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  })
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style='auto' />
+    </>
+  )
+}
